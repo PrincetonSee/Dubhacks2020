54d9cc4e359e1594a41493723076f204f7c2afd3
Donavan See <donavan@see-empire.com>
2020-10-18 02:40:09 -0700

    demo file
    Created main_test for demo purposes

--- Main.py
+++ Main.py
@@ -3,21 +3,23 @@ import TexttoSpeech as ts
 import SpeechToText as st
 from symptoms import Symptoms
 import RecordAudio
+import time
 import os
 
 
+start = time.perf_counter()
 # Set up symptoms object
 symptoms = Symptoms("symptoms.csv")
 
 # Opening Question
 playsound(ts.convert_to_speech("Welcome back Bob, what can I do to help?"))
-userInput = RecordAudio.get_recording(5)
+userInput = RecordAudio.get_recording(2)
 result = st.speech_to_text(userInput)
 print(result)
 
 # Inquire about symptoms
 playsound(ts.convert_to_speech("What are your symptoms?"))
-userInput = RecordAudio.get_recording(5)
+userInput = RecordAudio.get_recording(2.5)
 result = st.speech_to_text(userInput)
 print(result)
 symptom_found = symptoms.find_symptom(result)
@@ -35,7 +37,7 @@ possibilities = symptoms.get_possibilities(symptom_found)
 
 # Ask about pain level
 playsound(ts.convert_to_speech("Rate the pain you feel on a scale of 1 to 10"))
-userInput = RecordAudio.get_recording(5)
+userInput = RecordAudio.get_recording(2.5)
 result = st.speech_to_text(userInput)
 print(result)
 pain_calculated = symptoms.find_pain(result)
@@ -43,7 +45,7 @@ pain_calculated = symptoms.find_pain(result)
 # Ask about pain level again if not between 1 - 10
 while pain_calculated is None:
     playsound(ts.convert_to_speech("Rate the pain you feel on a scale of 1 to 10"))
-    userInput = RecordAudio.get_recording(5)
+    userInput = RecordAudio.get_recording(2.5)
     result = st.speech_to_text(userInput)
     print(result)
     pain_calculated = symptoms.convert_string_to_num(result)
@@ -54,30 +56,5 @@ diagnosis = symptoms.make_diagnosis(possibilities, pain_calculated)
 # Output final diagnosis
 playsound(ts.convert_to_speech("Our analysis shows that you may have " + diagnosis))
 
-
-
-# playsound(ts.convert_to_speech("Welcome back Bob, what can I do to help?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("What're your symptoms?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("Rate the pain you feel on a scale of 1 to 10"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("Do you feel any lumps on your throat?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("Our analysis shows that you may have Strep Throat. Would you like to hold for a doctor "
-#                                "to give you a prescription or visit a local free clinic?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
+end = time.perf_counter()
+print(end - start)
--- Main_test.py
+++ Main_test.py
@@ -1,42 +1,79 @@
-import sounddevice as sd
-import soundfile as sf
-import TexttoSpeech as ts
-# import SpeechToText as st
-import RecordAudio
-import os
+# THIS FILE IS FOR DEMO PURPOSES ONLY
 
-filename = ts.convert_to_speech("Welcome back Bob, what can I do to help?")
-data, fs = sf.read(filename, dtype='float32')
-sd.play(data, fs)
-status = sd.wait()  # Wait until file is done playing
-# playsound(ts.convert_to_speech("Welcome back Bob, what can I do to help?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("What're your symptoms?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("Rate the pain you feel on a scale of 1 to 10"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("Do you feel any lumps on your throat?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# playsound(ts.convert_to_speech("Our analysis shows that you may have Strep Throat. Would you like to hold for a doctor "
-#                                "to give you a prescription or visit a local free clinic?"))
-#
-# userInput = RecordAudio.get_recording(5)
-# print(st.speech_to_text(userInput))
-#
-# filename = 'myfile.wav'
-# # Extract data and sampling rate from file
-# data, fs = sf.read(filename, dtype='float32')
-# sd.play(data, fs)
-# status = sd.wait()  # Wait until file is done playing
+from playsound import playsound
+import TexttoSpeech as ts
+import SpeechToText as st
+from symptoms import Symptoms
+import RecordAudio
+import time
+
+
+# Set up symptoms object
+symptoms = Symptoms("symptoms.csv")
+
+# Opening Question
+playsound(ts.convert_to_speech("How can I help you?"))
+print("recording...")
+# D: My throat hurts
+userInput = RecordAudio.get_recording(2)
+result = st.speech_to_text(userInput)
+print(result)
+
+# Inquire about symptoms
+playsound(ts.convert_to_speech("What are your symptoms?"))
+print("recording...")
+# # D: I'm hot and my throat feels scratchy
+userInput = RecordAudio.get_recording(2.5)
+result = st.speech_to_text(userInput)
+print(result)
+symptom_found = symptoms.find_symptom(result)
+
+# Ask about symptoms again if symptom not found
+while symptom_found is None:
+    playsound(ts.convert_to_speech("What are your symptoms?"))
+    userInput = RecordAudio.get_recording(5)
+    result = st.speech_to_text(userInput)
+    print(result)
+    symptom_found = symptoms.find_symptom(result)
+
+# Look up and save possible diagnosis possibilities
+possibilities = symptoms.get_possibilities(symptom_found)
+
+# Ask about pain level
+playsound(ts.convert_to_speech("How bad is the pain from 1 to 10?"))
+print("recording...")
+# # D: Feels like a 6
+userInput = RecordAudio.get_recording(2.5)
+result = st.speech_to_text(userInput)
+print(result)
+# pain_calculated = symptoms.find_pain(result)
+
+# Ask about pain level again if not between 1 - 10
+# while pain_calculated is None:
+#     playsound(ts.convert_to_speech("Rate the pain you feel on a scale of 1 to 10"))
+#     userInput = RecordAudio.get_recording(2.5)
+#     result = st.speech_to_text(userInput)
+#     print(result)
+#     pain_calculated = symptoms.convert_string_to_num(result)
+
+# Calculate and save final diagnosis
+# diagnosis = symptoms.make_diagnosis(possibilities, pain_calculated)
+
+# Output final diagnosis
+playsound(ts.convert_to_speech("Our analysis shows that you may have " + "strep throat"))
+
+playsound(ts.convert_to_speech("Would you like to hold for a doctor to give you a prescription or visit a local free clinic?"))
+print("recording...")
+# # D: Visit a free clinic
+userInput = RecordAudio.get_recording(2.5)
+result = st.speech_to_text(userInput)
+print(result)
+
+playsound(ts.convert_to_speech("The nearest clinic is Rotacare free clinic, would you like to see the route or view other options?"))
+print("recording...")
+# # D: See route
+userInput = RecordAudio.get_recording(2)
+result = st.speech_to_text(userInput)
+print(result)
+
+playsound(ts.convert_to_speech("If you would like to send the report to the doctor click the left button, otherwise you may continue and print the directions"))
